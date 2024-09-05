5cd242a96877f383a680247c35adbcaa71faaa9b
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-05 16:05:33 -0600

    Creacion de las funciones de insertar y seleccionar registros

--- ejercicios/003_postgresql/Postgresql/src/main/scala/Learning.scala
+++ ejercicios/003_postgresql/Postgresql/src/main/scala/Learning.scala
@@ -4,6 +4,7 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Success, Failure}
+import java.util.UUID
 import java.sql.Timestamp
 import java.time.LocalDateTime
 
@@ -40,19 +41,16 @@ object Learning {
          |""".stripMargin)
 
     lazy val db = Database.forURL(url = host, user = usuario, password = password)
-    crearTablas(db)
+    //crearTablas(db)
 
-    insertaRegistros(db)
+    //insertaRegistros(db)
+
+    consultaRegistros(db)
 
 
 
-//
-//    // Consultar los datos
-//    val queryFuture = db.run(proCrontol.result)
-//
-//    queryFuture.map { result =>
-//      result.foreach(println)
-//    }
+
+
 
 //    val setup = db.run(DBIO.seq(
 //      schema.createIfNotExists
@@ -91,23 +89,53 @@ object Learning {
   /** Complete esta función para insertar los registros especificados */
   def insertaRegistros(db:Database): Unit = {
     println("Insertar registros")
-//    val proCrontol = TableQuery[ProcesoControlTable]
-//    try{
-//      val sql = DBIO.seq(
-//        proControl  += (0, Timestamp.valueOf(LocalDateTime.now()), "1", true, "test1.txt"),
-//        proControl  += (0, Timestamp.valueOf(LocalDateTime.now()), "2", true, "test2.txt")
-//      )
-//
-//      val sql1 = sql.insertStatement
-//
-//    }catch {
-//      case e: Exception =>
-//        println(s"Error al insertar el registro en la tabla: ${e.getMessage}")
-//    }
+    val proControl = TableQuery[ProcesoControlTable]
+    val uuid: String = UUID.randomUUID().toString
+    println(s"Identificador unico ${uuid}")
+    try{
+      // Definir los registros a insertar
+      val insertActions = DBIO.seq(
+        proControl += ProcesoControl(0, Timestamp.valueOf(LocalDateTime.now()), uuid, true, "test1.txt"),
+        proControl += ProcesoControl(0, Timestamp.valueOf(LocalDateTime.now()), uuid, true, "test2.txt")
+      )
+
+      // Ejecutar las inserciones
+      db.run(insertActions).onComplete {
+        case Success(_) => println("Registros insertados correctamente.")
+        case Failure(e) => println(s"Error al insertar registros: ${e.getMessage}")
+      }
+
+    }catch {
+      case e: Exception =>
+        println(s"Error al insertar el registro en la tabla: ${e.getMessage}")
+    }
   }
 
   /** Complete esta función para obtener los registros de la tabla [[Table]] */
-  def consultaRegistros() = ???
+  def consultaRegistros(db:Database): Unit = {
+    println("Consultar registros")
+    val proControl = TableQuery[ProcesoControlTable]
+    try {
+    val query = proControl.result
+
+    val result = db.run(query)
+
+    result.onComplete {
+      case Success(registros) =>
+        println("Registros en la tabla:")
+        registros.foreach { registro =>
+          println(s"ID: ${registro.id}, Fecha: ${registro.fechaActualizacion}, Identificador: ${registro.identificador}, Carga Correcta: ${registro.cargaCorrecta}, Archivo: ${registro.nombreArchivo}")
+        }
+      case Failure(e) =>
+        println(s"Error al consultar registros: ${e.getMessage}")
+    }
+      Await.result(result, 10.seconds)
+
+    } catch {
+      case e: Exception =>
+        println(s"Error al realizar la consulta: ${e.getMessage}")
+    }
+  }
 
   /** Complete esta función para actualizar los registros insertados */
   def actualizaRegistro(nombreArchivo: String, cargaCorrecta: Boolean): Unit =
